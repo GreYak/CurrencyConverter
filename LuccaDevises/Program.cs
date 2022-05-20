236cabee16af3498380969a2a741ee125d13ad82
Yann <grebot.yannick@gmail.com>
2022-05-21 01:27:05 +0200

    Lecture du fichier d'entrée

--- LuccaDevises/Program.cs
+++ LuccaDevises/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LuccaDevises.Tools;
+
+args = new string[1] { "./FilesFolder/Sample.csv"};             // TODO : supprimer.
+
+if (args.Length != 1)
+{
+    Console.Error.WriteLine("Un chemin de fichier doit être spécifier.");
+}
+else
+{
+    // Define the cancellation token.
+    CancellationTokenSource source = new CancellationTokenSource();
+    CancellationToken token = source.Token;
+
+    try
+    {
+        var fileParser = new FileParser(args[0]);
+        await foreach (string currentLine in fileParser.ReadContent(token))
+        {
+            Console.WriteLine(currentLine);
+        }
+    }
+    catch(Exception exc)
+    {
+        source.Cancel();
+        Console.Error.WriteLine($"Le fichier fourni n'a pas un format valide => {exc.Message}");
+    }
+    finally
+    {
+        source.Dispose();
+    }
+}
+
---
+++ LuccaDevises/Tools/FileParser.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LuccaDevises.Tools
+{
+    /// <summary>
+    /// Parser of file, to extract its content. 
+    /// </summary>
+    internal class FileParser
+    {
+        private readonly string _filePath;
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="FileParser"/>
+        /// </summary>
+        /// <param name="filePath">The path of the file to parse.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="filePath"/> doesn't exist.</exception>
+        public FileParser(string filePath)
+        {
+            ArgumentNullException.ThrowIfNull(filePath, nameof(filePath));
+            if (!File.Exists(filePath))
+                throw new ArgumentException($"File '{filePath}' doesn't exist.");
+            _filePath = filePath;
+        }
+
+        /// <summary>
+        /// Extract the file content, line after line.
+        /// </summary>
+        /// <returns>File's lines.</returns>
+        public async IAsyncEnumerable<string?> ReadContent([EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            using (var streamReader = new StreamReader(File.OpenRead(_filePath)))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        yield break; 
+                    }
+                    yield return await streamReader.ReadLineAsync();
+                }
+
+                streamReader.Close();
+            }
+        }
+    }
+}
